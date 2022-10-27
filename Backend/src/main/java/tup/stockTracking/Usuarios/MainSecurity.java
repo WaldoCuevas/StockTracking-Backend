0c01febe0c96fdee2d3f6ff4ab6c693071d537a3
WaldoCuevas <104439067+WaldoCuevas@users.noreply.github.com>
2022-10-27 15:11:14 -0300

    WebConfiguration
    Clase deprecada WebConfiguration actualizada.

--- Backend/src/main/java/tup/stockTracking/Usuarios/MainSecurity.java
+++ Backend/src/main/java/tup/stockTracking/Usuarios/MainSecurity.java
@@ -11,16 +11,16 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class MainSecurity extends WebSecurityConfigurerAdapter {
+public class MainSecurity {
 
     @Autowired
     UserDetailsServiceImpl userDetailsService;
@@ -29,33 +29,75 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
     JwtEntryPoint jwtEntryPoint;
 
     @Bean
-    public JwtTokenFilter jwtTokenFilter(){
+    public JwtTokenFilter jwtTokenFilter() {
         return new JwtTokenFilter();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
+    // Metodos de WebSecurityConfigurerAdapter
+    /*
+     * @Override
+     * protected void configure(AuthenticationManagerBuilder auth) throws Exception
+     * {
+     * auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder()
+     * );
+     * }
+     * 
+     * @Bean
+     * 
+     * @Override
+     * public AuthenticationManager authenticationManagerBean() throws Exception {
+     * return super.authenticationManagerBean();
+     * }
+     * 
+     * @Override
+     * protected AuthenticationManager authenticationManager() throws Exception {
+     * return super.authenticationManager();
+     * }
+     * 
+     * @Override
+     * protected void configure(HttpSecurity http) throws Exception {
+     * http.cors().and().csrf().disable()
+     * .authorizeRequests()
+     * .antMatchers("/auth/**").permitAll()
+     * .anyRequest().authenticated()
+     * .and()
+     * .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
+     * .and()
+     * .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+     * http.addFilterBefore(jwtTokenFilter(),
+     * UsernamePasswordAuthenticationFilter.class);
+     * }
+     */
+
+    // Nueva implementacion
+
+    // AuthenticationManager es reemplazado por UserDetailsManager o
+    // UserDetailsService
+    /*
+     * Como nosotros tenemos crear un servicio de UserDetailService. podemos implementar la 2da opcion
+     */
 
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public AuthenticationManager authManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder,
+            UserDetailsServiceImpl userDetailService)
+            throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and()
+                .build();
     }
 
-    @Override
-    protected AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
-    }
+    // Configure HttpSecurity es reemplzado por SecurityFilterChain
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
@@ -65,6 +107,8 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
-    
+
 }
